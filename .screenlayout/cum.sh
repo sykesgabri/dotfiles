4528a49d15e401706876082e34f2a89e035fc537
Gabriel Sykes <gabrielsykes30@gmail.com>
2024-08-24 20:56:22 +0100

    Add .screenlayout/ directory

---
+++ .screenlayout/cum.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DisplayPort-0 --primary --mode 2560x1440 --pos 1920x1440 --rotate normal --output DisplayPort-1 --off --output DisplayPort-2 --mode 3440x1440 --pos 1480x0 --rotate normal --output HDMI-A-0 --mode 1920x1080 --pos 0x1620 --rotate normal
